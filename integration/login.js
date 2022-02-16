89eb3c058c520d270bcecab1b7299d2acbd5d2a0
pac <ppppacchan@gmail.com>
2022-02-16 21:23:05 +1300

    almost there

--- integration/login.js
+++ integration/login.js
@@ -4,19 +4,29 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-describe('log in', function () {
-    it('log in to home page and browse to edit profile page', function(){
+describe('Qurious interview', function () {
+    it('log in to home page and browses to edit profile page', function(){
         cy.visit('http://13.75.189.136:8081')
         cy.get('#username').type('ubiquity')
         cy.get('#password').type('P@ss123#UbiQuity')
         cy.get('.btn').click()
     })
 
-    it('verify edit profile page is opened', function(){
+    it('Verify user can go to edit profile page successfully', function(){
         cy.get('[href="/editprofile"]').click()
-        cy
-            .get('#IntroText > :nth-child(1)')
-            .should('be.visible')
+        cy.get('#IntroText > :nth-child(1)').should('be.visible')
+    })
+
+    it('User successfully changes email address, date of birth and password', function(){
+        const d = new Date() 
+        cy.get('#Email > .field-value > .textfield').clear()
+        .type('qa+'+ d.getTime('en-GB') + '@test.co.nz')
+        cy.get('#DateOfBirth > .field-value > .textfield').clear()
+        .type(d.toLocaleDateString('en-GB'))
+        cy.get('#NewPassword > .field-value > .textfield').type('P@ss123#UbiQuity')
+        cy.get('#ConfirmNewPassword > .field-value > .textfield').type('P@ss123#UbiQuity')
+        cy.get('.submit-button').click()
+        cy.get('#SuccessMessage').should('be.visible')
     })
 
 })
