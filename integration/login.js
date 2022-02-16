2591245f7706b0e0723dffa54db94466caf03116
pac <ppppacchan@gmail.com>
2022-02-16 21:03:05 +1300

    updated 01

--- integration/login.js
+++ integration/login.js
@@ -5,12 +5,18 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 describe('log in', function () {
-    it('log in to home page', function(){
+    it('log in to home page and browse to edit profile page', function(){
         cy.visit('http://13.75.189.136:8081')
         cy.get('#username').type('ubiquity')
         cy.get('#password').type('P@ss123#UbiQuity')
         cy.get('.btn').click()
+    })
+
+    it('verify edit profile page is opened', function(){
         cy.get('[href="/editprofile"]').click()
+        cy
+            .get('#IntroText > :nth-child(1)')
+            .should('be.visible')
     })
 
 })
